f958ff20d657a1359ec1c75a1e2d48c2753a1f72
Code-with-rawat <rawathimanshu2005r@gmail.com>
2025-09-05 22:26:01 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Cards from "./components/Cards";
-// import Navbar from "./components/Navbar";
 import Navbar from "./Project/Todo/Navbar";
-// import "./App.css";
-// import State from './components/hooks/State';
-// import DeviceState from './components/hooks/DeviceState';
-// import ToggleSwitch from "./Project/Toggle/ToggleSwitch";
 import Todo from "./Project/Todo/Todo";
 export default function App() {
-  // const [Dark, setDark] = useState('light');
-  // const toggleBtn = () => {
-  //   if (Dark === 'light') {
-  //     setDark('black');
-  //     document.body.style.backgroundColor = '#042743';
-  //   } else {
-  //     setDark('light');
-  //     document.body.style.backgroundColor = '#ffffff';
-  //   }
-  //};
-
-  // const [Darks, setDarks] = useState("light");
-   const [mode, setMode] = useState(() => {
+  const [mode, setMode] = useState(() => {
     return localStorage.getItem("theme") || "light";
   });
-  
+
   useEffect(() => {
     localStorage.setItem("theme", mode);
   }, [mode]);
@@ -38,7 +21,7 @@ export default function App() {
   //   }
   // };
 
-   const toggleMode = () => {
+  const toggleMode = () => {
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
 
@@ -50,9 +33,10 @@ export default function App() {
       {/* <State/> */}
       {/* <DeviceState/> */}
       {/* <ToggleSwitch/> */}
-       <div className={`app bg-${mode} text-${mode === "light" ? "dark" : "light"}`}>
-      <Navbar mode={mode} toggleBtn={toggleMode} />
-      <Todo mode={mode}/>
+      <div
+        className={`app bg-${mode} text-${mode === "light" ? "dark" : "light"}`}>
+        <Navbar mode={mode} toggleBtn={toggleMode} />
+        <Todo mode={mode} />
       </div>
     </>
   );
